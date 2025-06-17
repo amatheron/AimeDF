846cbd3e16ed64bce8526863e4cc24851779fc12
Aime Matheron <a.matheron@hi-jena.gsi.de>
2025-06-17 10:22:42 +0200

    test with Bayesian opt

--- src/darkfield/dfdf_Aime.py
+++ src/darkfield/dfdf_Aime.py
@@ -230,7 +230,8 @@ for positive_signal_simulation in positive_signal_simulations:
         flow_figs = 0
         gyax = df.yamlval('flow_plot_gyax', ip['simulation'], [-200, 1000, 10])
         clim = df.yamlval('flow_plot_clim', ip['simulation'], [1e-11, 50])
+        print(str(projectdir.relative_to('/home/yu79deg')), fn )
         #df.flow_plot(str(projectdir), fn , flow_figs=flow_figs, gyax_def=gyax, cl=clim)
-        #df.flow_plot(str(projectdir.relative_to('/home/yu79deg')), fn , flow_figs=flow_figs, gyax_def=gyax, cl=clim)
+        df.flow_plot(str(projectdir.relative_to('/home/yu79deg')), fn , flow_figs=flow_figs, gyax_def=gyax, cl=clim)
 
 print('Simulation finished.')
